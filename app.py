302d9379b8c2758d05f5f3dc02da0a641705d60e
polarbio <157435845+polarbio@users.noreply.github.com>
2025-05-28 22:00:04 -0500

    xdc

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Login CMCH con Google")
 
 if st.button("Ingresar"):
-    st.login("google")
+    st.login("auth0")
 
 st.json(st.experimental_user())  # Mostrar información del usuario autenticado
 st.image(st.experimental_user().picture, width=100)  # Mostrar la foto de perfil del usuario
--- main.py
+++ main.py
@@ -1,51 +1,10 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
-import auth
 
-def main():
-    if "token" not in st.session_state:
-        st.session_state.token = None
-    if "oauth_state" not in st.session_state:
-        st.session_state.oauth_state = None
+st.title("Login CMCH con Google")
 
-    if not st.session_state.get("token"):
-        auth.login_box()
-        return
+if st.button("Ingresar"):
+    st.login("auth0")
 
-    userinfo = auth.get_user_info()
-    if userinfo:
-        email = userinfo.get("email")
-
-        if not auth.is_authorized_user(email):
-            st.error("🚫 Acceso denegado. Tu cuenta no está autorizada.")
-            if st.button("Cerrar sesión"):
-                auth.logout()
-            return
-
-        role = auth.get_user_role(email)
-        st.success(f"Bienvenido, {role} 👋")
-
-        if st.button("Cerrar sesión"):
-            auth.logout()
-            return
-
-        with st.sidebar:
-            menu = option_menu(
-                "Menú principal",
-                ["Inicio", "Ver Base de Datos", "Generar QR", "Configuración"],
-                icons=['house', 'table', 'qr-code', 'gear'],
-                menu_icon="cast",
-                default_index=0
-            )
-
-        if menu == "Inicio":
-            vista_datos.mostrar_inicio()
-        elif menu == "Ver Base de Datos":
-            base_datos.mostrar_base_datos()
-        elif menu == "Generar QR":
-            generar_qr.generar_qrs()
-        elif menu == "Configuración":
-            st.info("⚙️ Configuración por implementar.")
-
-if __name__ == "__main__":
-    main()
+st.json(st.experimental_user())  # Mostrar información del usuario autenticado
+st.image(st.experimental_user().picture, width=100)  # Mostrar la foto de perfil del usuario
+st.logout("Cerrar sesión")  # Botón para cerrar sesión
