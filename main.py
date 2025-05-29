bb8f36a200a1246fcd274f83df21733b5d902c23
polarbio <157435845+polarbio@users.noreply.github.com>
2025-05-28 22:08:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ st.title("Login CMCH con Google")
 if st.button("Ingresar"):
     st.login("auth0")
 
-st.json(st.experimental_user())  # Mostrar información del usuario autenticado
-st.image(st.experimental_user().picture, width=100)  # Mostrar la foto de perfil del usuario
+#st.json(st.experimental_user())  # Mostrar información del usuario autenticado
+#st.image(st.experimental_user().picture, width=100)  # Mostrar la foto de perfil del usuario
 st.logout("Cerrar sesión")  # Botón para cerrar sesión
